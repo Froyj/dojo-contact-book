fca0c2baf1cec2f603b2461e0ba145c427902556
Geoffroy Periot <geoffroy.periot.dev@gmail.com>
2021-04-20 10:27:31 +0200

    remove func

--- getContactBook.js
+++ getContactBook.js
@@ -1,17 +1,6 @@
 // Write your code here
 function getContactBook (people) {
-  const formattedPeople = people.map(p => {
-    return { fullName: `${p.firstName} ${p.lastName}`, phone: p.info.phone }
-  })
-  const contactBook = {};
-  for(let p of formattedPeople) {
-    if(!contactBook.hasOwnProperty(p.fullName[0])) {
-      contactBook[p.fullName[0]] = []
-    } 
-    contactBook[p.fullName[0]].push(p)
-  }
-  console.log(contactBook)
-  return contactBook;
+
 } 
 
 
